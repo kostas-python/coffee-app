74b5108281ebfc5a404b0eae25639cc0e825c02f
kostas-python <diamantopoulos.kostas@hotmail.com>
2023-12-31 20:06:01 +0000

    Added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../assets/cityzen1.jpg';
+
+function navbar() {
+  return (
+    <div className="navbar">
+        <div className="leftSide">
+            <img src={Logo}></img>
+        </div>
+        <div className="rightSideSide">
+        </div>
+    </div>
+  );
+}
+
+export default navbar
