a80e1b179f930954a6f2e614455b271310372f9a
kostas-python <diamantopoulos.kostas@hotmail.com>
2024-01-04 22:11:04 +0000

    initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,10 +20,7 @@ function About() {
           officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
           nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
           error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
+          officiis modi at sunt excepturi expedita sint? 
         </p>
       </div>
     </div>
